78595a6fff75d83a8457475cba817d312bba53fa
Sheriefs Gamble <sgamble284@gmail.com>
2020-02-05 09:54:21 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,6 +3,11 @@ class Student
 
   def self.new_from_db(row)
     # create a new Student object given a row from the database
+    new_student = self.new
+    new_student.id = row[0]
+    new_student.name = row[1]
+    new_student.grade = row[2]
+    new_student
   end
 
   def self.all
@@ -13,6 +18,74 @@ class Student
   def self.find_by_name(name)
     # find the student in the database given a name
     # return a new instance of the Student class
+    sql = <<-SQL
+      SELECT * FROM students
+      WHERE name = ?
+      LIMIT 1
+    SQL
+    
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+  end.first
+end
+
+def self.all
+  sql = <<-SQL
+    SELECT * FROM students
+    SQL
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
+  end
+  
+  def self.all_students_in_grade_9
+    students_in_grade_9 = []
+    sql = <<-SQL
+      SELECT * FROM students
+      WHERE grade = 9
+      SQL
+      DB[:conn].execute(sql).map do |row|
+        students_in_grade_9 << self.new_from_db(row)
+        # binding.pry
+      end
+      students_in_grade_9
+      
+  end
+  
+  def self.students_below_12th_grade
+    students_below_grade_12 = []
+    sql = <<-SQL
+      SELECT * FROM students
+      WHERE grade < 12
+      SQL
+
+      DB[:conn].execute(sql).map do |row|
+        students_below_grade_12 << self.new_from_db(row)
+      end
+      
+      students_below_grade_12
+      # binding.pry
+  end
+  
+  def self.first_student_in_grade_10
+    sql = <<-SQL
+      SELECT * FROM students
+      WHERE grade = 10
+      LIMIT 1
+      SQL
+
+      DB[:conn].execute(sql).map do |row|
+        self.new_from_db(row)
+      end.first
+  end
+  
+  def self.first_X_students_in_grade_10
+    sql = <<-SQL
+      SELECT * FROM studetns
+      WHERE grade = 10
+      LIMIT ?
+      SQL
+      
   end
   
   def save
